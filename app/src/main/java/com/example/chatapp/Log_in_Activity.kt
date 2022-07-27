cc50def01429d9d168451ed6de01a2cef1cd4437
Bermet <bermet.bainaeva.ch@gmail.com>
2022-07-27 21:55:10 +0600

    Chat app log in part

---
+++ app/src/main/java/com/example/chatapp/Log_in_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.chatapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Log_in_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
