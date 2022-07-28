2435382286e65f0340bda84406463214c484a5e5
Bermet <bermet.bainaeva.ch@gmail.com>
2022-07-28 19:54:47 +0600

    again gradle problems

--- app/src/main/java/com/example/chatapp/Log_in_Activity.kt
+++ app/src/main/java/com/example/chatapp/Log_in_Activity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class Log_in_Activity : AppCompatActivity() {
 
@@ -12,10 +14,13 @@ class Log_in_Activity : AppCompatActivity() {
     private lateinit var etPassword:EditText
     private lateinit var btnLog:Button
     private lateinit var btnSign:Button
+
+
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
-
+     mAuth= FirebaseAuth.getInstance()
         etEmail=findViewById(R.id.et_email)
         etPassword=findViewById(R.id.et_password)
         btnLog=findViewById(R.id.btn_log_in)
@@ -28,7 +33,27 @@ val intent=Intent(this,SignUpActivity::class.java)
 
         }
 
+     btnLog.setOnClickListener {
+         val email = etEmail.text.toString()
+         val password =etPassword.text.toString()
 
+
+         login(email,password)
+     }
+
+    }
+    private  fun login(email:String, password:String){
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent=Intent(this@Log_in_Activity,MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    Toast.makeText(this@Log_in_Activity, "User does not exist", Toast.LENGTH_LONG).show()
+
+                }
+            }
 
     }
 }
--- app/src/main/java/com/example/chatapp/SignUpActivity.kt
+++ app/src/main/java/com/example/chatapp/SignUpActivity.kt
@@ -1,11 +1,59 @@
 package com.example.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var name:EditText
+
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnSign: Button
+    private lateinit var mAuth2: FirebaseAuth
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        name = findViewById(R.id.et_name2)
+        etEmail = findViewById(R.id.et_email2)
+        etPassword = findViewById(R.id.et_password2)
+        btnSign = findViewById(R.id.btn_sign_in2)
+
+        btnSign.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+
+            signUp(email, password)
+        }
+
+
     }
-}
+    private fun  signUp(email: String, password: String){
+        mAuth2.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent=Intent(this@SignUpActivity,MainActivity::class.java)
+                    startActivity(intent)
+
+
+                } else {
+                    Toast.makeText(this@SignUpActivity,"some error occurred", Toast.LENGTH_SHORT).show()
+
+
+                }
+            }
+
+
+    }
+
+    }
+
