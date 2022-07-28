79430bbd67bae2f22663ba4e077a51f4784c3bd2
Bermet <bermet.bainaeva.ch@gmail.com>
2022-07-28 14:26:16 +0600

    remove bug

--- app/src/main/java/com/example/chatapp/Log_in_Activity.kt
+++ app/src/main/java/com/example/chatapp/Log_in_Activity.kt
@@ -3,14 +3,15 @@ package com.example.chatapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 
 class Log_in_Activity : AppCompatActivity() {
 
     private lateinit var etEmail:EditText
     private lateinit var etPassword:EditText
-    private lateinit var btnLog:EditText
-    private lateinit var btnSign:EditText
+    private lateinit var btnLog:Button
+    private lateinit var btnSign:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
