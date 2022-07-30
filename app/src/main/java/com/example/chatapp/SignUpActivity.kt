0c7784e9240282028e4c974af67982d04d76c806
Bermet <bermet.bainaeva.ch@gmail.com>
2022-07-30 18:32:19 +0600

    30.07

--- app/src/main/java/com/example/chatapp/SignUpActivity.kt
+++ app/src/main/java/com/example/chatapp/SignUpActivity.kt
@@ -7,40 +7,45 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 
 class SignUpActivity : AppCompatActivity() {
 
-    private lateinit var name:EditText
+    private lateinit var etName:EditText
 
     private lateinit var etEmail: EditText
     private lateinit var etPassword: EditText
     private lateinit var btnSign: Button
     private lateinit var mAuth2: FirebaseAuth
+    private lateinit var dbInfo: DatabaseReference
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-        name = findViewById(R.id.et_name2)
+        etName = findViewById(R.id.et_name2)
         etEmail = findViewById(R.id.et_email2)
         etPassword = findViewById(R.id.et_password2)
         btnSign = findViewById(R.id.btn_sign_in2)
 
         btnSign.setOnClickListener {
+            val name=etName.text.toString()
             val email = etEmail.text.toString()
             val password = etPassword.text.toString()
 
 
-            signUp(email, password)
+            signUp(email, password,name)
         }
 
 
     }
-    private fun  signUp(email: String, password: String){
+    private fun  signUp(name:String, email: String, password: String){
         mAuth2.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
+
+                    addUserToDatabase(name,email,mAuth2.currentUser?.uid!!)
                     val intent=Intent(this@SignUpActivity,MainActivity::class.java)
                     startActivity(intent)
 
@@ -52,6 +57,10 @@ class SignUpActivity : AppCompatActivity() {
                 }
             }
 
+
+    }
+
+    private fun addUserToDatabase(name: String, email: String, uid:String){
 
     }
 
